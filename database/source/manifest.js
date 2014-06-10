fd3afa0bbecbc778c7d6ccd26b825b288e50989f
Steve Hackbarth <stephenhackbarth@gmail.com>
2014-06-10 09:48:19 -0400

    brought in ice cream extension

---
+++ database/source/manifest.js
@@ -0,0 +1,13 @@
+{
+  "name": "icecream",
+  "version": "1.8.0",
+  "comment": "Ice Cream extension",
+  "loadOrder": 999,
+  "dependencies": ["crm"],
+  "databaseScripts": [
+    "create_ic_schema.sql",
+    "icflav.sql",
+    "priv.sql",
+    "cntcticflav.sql"
+  ]
+}
